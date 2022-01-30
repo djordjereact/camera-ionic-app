47a134036e284b6d1da38d3ebf76c4062a3154e8
Dusan Mihailovic <duks05@yahoo.com>
2022-01-30 18:05:08 +0100

    Added some new components to the new memory page

--- src/pages/NewMemory.tsx
+++ src/pages/NewMemory.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
-import {IonButtons, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonBackButton} from "@ionic/react";
+import {
+    IonButtons,
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+    IonBackButton,
+    IonGrid,
+    IonRow, IonCol, IonItem, IonLabel, IonInput, IonButton, IonIcon
+} from "@ionic/react";
+import {camera} from "ionicons/icons";
+import './NewMemory.css'
 
 const NewMemory: React.FC = () => {
     return (
@@ -13,7 +25,32 @@ const NewMemory: React.FC = () => {
                 </IonToolbar>
             </IonHeader>
             <IonContent>
-                <h2>Add New Memory</h2>
+                <IonGrid>
+                    <IonRow>
+                        <IonCol>
+                            <IonItem>
+                                <IonLabel position="floating">Memory Title</IonLabel>
+                                <IonInput type="text"></IonInput>
+                            </IonItem>
+                        </IonCol>
+                    </IonRow>
+                    <IonRow className="ion-text-center">
+                        <IonCol>
+                            <div className="image-preview">
+                                <h3>No photo chosen</h3>
+                            </div>
+                            <IonButton fill="clear">
+                                <IonIcon icon={camera} slot="start"/>
+                                <IonLabel>Take Photo</IonLabel>
+                            </IonButton>
+                        </IonCol>
+                    </IonRow>
+                    <IonRow className="ion-margin-top">
+                        <IonCol className="ion-text-center">
+                            <IonButton>Add Memory</IonButton>
+                        </IonCol>
+                    </IonRow>
+                </IonGrid>
             </IonContent>
         </IonPage>
     )
