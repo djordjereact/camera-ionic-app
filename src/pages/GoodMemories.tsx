10f15cc714a64ded2111e3ece33ed0403bf84827
Dusan Mihailovic <duks05@yahoo.com>
2022-01-30 17:54:14 +0100

    Added toolbar

--- src/pages/GoodMemories.tsx
+++ src/pages/GoodMemories.tsx
@@ -1,16 +1,40 @@
 import React from 'react';
-import {IonContent, IonHeader, IonPage, IonTitle, IonToolbar} from "@ionic/react";
+import {
+    IonButton,
+    IonButtons,
+    IonContent, IonFab, IonFabButton,
+    IonHeader,
+    IonIcon,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+    isPlatform
+} from "@ionic/react";
+import {add} from "ionicons/icons";
 
 const GoodMemories: React.FC = () => {
     return (
         <IonPage>
             <IonHeader>
                 <IonToolbar>
+                    {isPlatform('ios') &&
+                    <IonButtons slot="end">
+                        <IonButton routerLink="/new-memory">
+                            <IonIcon slot="icon-only" icon={add}/>
+                        </IonButton>
+                    </IonButtons>}
                     <IonTitle>Good Memories</IonTitle>
                 </IonToolbar>
             </IonHeader>
             <IonContent>
                 <h2>Good Memories</h2>
+                {!isPlatform('ios') &&
+                    <IonFab vertical="bottom" horizontal="end">
+                        <IonFabButton routerLink="/new-memory">
+                            <IonIcon icon={add}/>
+                        </IonFabButton>
+                    </IonFab>
+                }
             </IonContent>
         </IonPage>
     )
--- src/pages/NewMemory.tsx
+++ src/pages/NewMemory.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import {IonContent, IonHeader, IonPage, IonTitle, IonToolbar} from "@ionic/react";
+import {IonButtons, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonBackButton} from "@ionic/react";
 
 const NewMemory: React.FC = () => {
     return (
         <IonPage>
             <IonHeader>
                 <IonToolbar>
+                    <IonButtons slot="start">
+                        <IonBackButton defaultHref="/good-memories" />
+                    </IonButtons>
                     <IonTitle>Add New Memory</IonTitle>
                 </IonToolbar>
             </IonHeader>
